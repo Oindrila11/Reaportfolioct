e8f46ee28ccce62d9a03dd59834a9cd3763b4ec0
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-07-12 11:00:21 -0500

    Updates

---
+++ precache-manifest.69f55e6527aaa43e9e674db6809679ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "16d64e91c992ebade6c7",
+    "url": "/Reaportfolioct/static/css/main.a6cd3bc7.chunk.css"
+  },
+  {
+    "revision": "16d64e91c992ebade6c7",
+    "url": "/Reaportfolioct/static/js/main.16d64e91.chunk.js"
+  },
+  {
+    "revision": "4315a229e48773adfa87",
+    "url": "/Reaportfolioct/static/js/1.4315a229.chunk.js"
+  },
+  {
+    "revision": "b8897f55e2d3b584530b",
+    "url": "/Reaportfolioct/static/js/runtime~main.b8897f55.js"
+  },
+  {
+    "revision": "15d594b328a70253bed582556662dc11",
+    "url": "/Reaportfolioct/static/media/budget-tracker.15d594b3.png"
+  },
+  {
+    "revision": "9eda332a42a4df58045a9923ce9732a9",
+    "url": "/Reaportfolioct/static/media/project1.9eda332a.png"
+  },
+  {
+    "revision": "7e68870d42b8df0f72e743b2906b9e94",
+    "url": "/Reaportfolioct/static/media/project2.7e68870d.png"
+  },
+  {
+    "revision": "de79ea512627a0f05bbebd7cbd9557b2",
+    "url": "/Reaportfolioct/static/media/weather.de79ea51.png"
+  },
+  {
+    "revision": "04c017c4991f5cd8f17b0ad848839f42",
+    "url": "/Reaportfolioct/index.html"
+  }
+];
